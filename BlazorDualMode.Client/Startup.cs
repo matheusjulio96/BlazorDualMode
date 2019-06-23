af4fde5c1c3cf4860e99a14e8f5360636913f739
matheusjulio96 <matheus.julio96@hotmail.com>
2019-06-23 19:21:04 -0300

    dualmode

--- BlazorDualMode.Client/Startup.cs
+++ BlazorDualMode.Client/Startup.cs
@@ -1,3 +1,5 @@
+using BlazorDualMode.Client.Data;
+using BlazorDualMode.Shared;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +9,7 @@ namespace BlazorDualMode.Client
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IWeatherForecastService, WeatherForecastService>();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
--- BlazorDualMode.Server/Startup.cs
+++ BlazorDualMode.Server/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
 using System.Linq;
+using BlazorDualMode.Server.Data;
+using BlazorDualMode.Shared;
 
 namespace BlazorDualMode.Server
 {
@@ -14,33 +16,40 @@ namespace BlazorDualMode.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddNewtonsoftJson();
+            services.AddMvc();
+            services.AddServerSideBlazor();
             services.AddResponseCompression(opts =>
             {
                 opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
                     new[] { "application/octet-stream" });
             });
+            services.AddSingleton<IWeatherForecastService, WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseResponseCompression();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBlazorDebugging();
             }
 
+            app.UseHttpsRedirection();
+
+            app.UseResponseCompression();
             app.UseClientSideBlazorFiles<Client.Startup>();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
-                endpoints.MapFallbackToClientSideBlazor<Client.Startup>("index.html");
+                if (env.IsDevelopment())
+                {
+                    endpoints.MapBlazorHub();
+                }
+                endpoints.MapFallbackToPage("/_Host");
             });
         }
     }
